253ee9847625c90928ebd416eec555a664d6fc3b
Sakshi Patil <73053880+sakshiprit@users.noreply.github.com>
2023-10-31 08:30:44 +0100

    A Sushi menu app embedded in list in SwiftUI

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Sakshi Patil on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
